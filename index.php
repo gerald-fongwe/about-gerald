49b066c6cd283453a34459b4d8cfd4f6522cda1d
gerald-fongwe <geraldjam2004@yahoo.com>
2015-10-13 11:44:26 -0600

    added about-gerald test page to PhpStorm

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-		<head>
-			<meta charset="utf-8"/>
-			<title>about-gerald</title>
-		</head>
-		<h1>The life of Gerald></h1>
-		<h2>Who is Gerald</h2>
-		<body>
-			<p>Story of Gerald birth</p>
-			<p>Where has Gerald Lived</p>
-			<p>What is Gerald doing now</p>
-		</body>
-	</html>
+	<head>
+		<meta charset="utf-8"/>
+		<title>about-gerald</title>
+	</head>
+	<body>
+	<h1>The life of Gerald></h1>
+	<h2>Who is Gerald</h2>
+		<p>Story of Gerald's birth</p>
+		<p>Where has Gerald Lived</p>
+		<p>Which schools Gerald has Attended</p>
+		<p>What is Gerald doing now</p>
+		<img src="img/superman-dcuo.jpg" alt="superman"/>
+	</body>
+</html>
